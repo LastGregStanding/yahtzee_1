4e06233e507581e6fd8f1aeba6f58066771f9b0a
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-08-01 19:52:53 -0700

    added keep button functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dice2 = document.querySelector("#dice_2");
 const dice3 = document.querySelector("#dice_3");
 const dice4 = document.querySelector("#dice_4");
 const dice5 = document.querySelector("#dice_5");
+const keepBtns = document.querySelectorAll(".keep");
 const dice = [];
 
 // Random number 1-6
@@ -14,15 +15,21 @@ const randomNumber = () => Math.floor(Math.random() * 6 + 1);
 const rollDice = function () {
   for (let i = 0; i < 5; i++) {
     let number = randomNumber();
-    dice.push(number);
+    dice[i] = number;
   }
 };
 
-rollDice();
-console.log(dice);
+rollBtn.addEventListener("click", function () {
+  rollDice();
+  dice1.src = `dice/dice-${dice[0]}.png`;
+  dice2.src = `dice/dice-${dice[1]}.png`;
+  dice3.src = `dice/dice-${dice[2]}.png`;
+  dice4.src = `dice/dice-${dice[3]}.png`;
+  dice5.src = `dice/dice-${dice[4]}.png`;
+});
 
-dice1.src = `dice/dice-${dice[0]}.png`;
-dice2.src = `dice/dice-${dice[1]}.png`;
-dice3.src = `dice/dice-${dice[2]}.png`;
-dice4.src = `dice/dice-${dice[3]}.png`;
-dice5.src = `dice/dice-${dice[4]}.png`;
+keepBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    btn.style.backgroundColor = "blue";
+  })
+);
