67df98d79cc3c599b97ca63d0a7500a865a9f012
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-08-01 20:07:23 -0700

    made my code cleaner

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 "use strict";
 
 const rollBtn = document.querySelector(".roll_btn");
-const dice1 = document.querySelector("#dice_1");
-const dice2 = document.querySelector("#dice_2");
-const dice3 = document.querySelector("#dice_3");
-const dice4 = document.querySelector("#dice_4");
-const dice5 = document.querySelector("#dice_5");
 const keepBtns = document.querySelectorAll(".keep");
 const dice = [];
 
@@ -13,23 +8,25 @@ const dice = [];
 const randomNumber = () => Math.floor(Math.random() * 6 + 1);
 
 const rollDice = function () {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 1; i < 6; i++) {
     let number = randomNumber();
+    let keepBtn = document.querySelector(`#keep_${i}`);
+    if (keepBtn.classList.contains("pressed")) {
+      continue;
+    }
     dice[i] = number;
+    let diceImage = document.getElementById(`dice_${i}`);
+    diceImage.src = `dice/dice-${dice[i]}.png`;
   }
 };
 
 rollBtn.addEventListener("click", function () {
   rollDice();
-  dice1.src = `dice/dice-${dice[0]}.png`;
-  dice2.src = `dice/dice-${dice[1]}.png`;
-  dice3.src = `dice/dice-${dice[2]}.png`;
-  dice4.src = `dice/dice-${dice[3]}.png`;
-  dice5.src = `dice/dice-${dice[4]}.png`;
 });
 
 keepBtns.forEach((btn) =>
   btn.addEventListener("click", function () {
     btn.style.backgroundColor = "blue";
+    btn.classList.add("pressed");
   })
 );
